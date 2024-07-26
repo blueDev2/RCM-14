3084dd00d3a07dec0206c95594d7aa9333c4209c
blue3212 <luckywill339@gmail.com>
2024-07-26 16:34:57 -0400

    Add boilerplate Client UI IntelClueDisplay

---
+++ Content.Client/_RMC14/Intelligence/UI/IntelClueDisplayBoundUserInterface.cs
@@ -0,0 +1,32 @@
+using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Content.Client._RMC14.Intelligence.UI;
+
+[UsedImplicitly]
+public sealed partial class IntelClueDisplayBoundUserInterface : BoundUserInterface
+{
+    private IntelClueDisplayWindow? _window;
+    public IntelClueDisplayBoundUserInterface(EntityUid owner, Enum key) : base(owner, key)
+    {
+    }
+    protected override void Open()
+    {
+        base.Open();
+        _window = new IntelClueDisplayWindow(this);
+
+        _window.OnClose += Close;
+        _window.OpenCentered();
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+        if (!disposing) return;
+        _window?.Dispose();
+    }
+}
